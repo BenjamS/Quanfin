3c225af1411e4d096426fa8f53c21206de5eb630
BenjamS <ben.schiek@gmail.com>
2018-03-19 20:05:19 -0500

    Update tsTrends.R

--- tsTrends.R
+++ tsTrends.R
@@ -1,4 +1,4 @@
-tsTrends <- function(in_ts, wind = 17, graph = T)
+tsTrends <- function(in_ts, slope_window = 13, before_window = 5, aft_window = 2, quietly = T)
 {
   #Captures index of sustained up and down trends of a time series
   #by identifying periods of consecutive time steps in which slope
@@ -8,7 +8,7 @@ tsTrends <- function(in_ts, wind = 17, graph = T)
   #---------------------
   #Make sure required packages are loaded
   #---------------------
-  required_packages <- c("pracma", "ggplot2")
+  required_packages <- c("pracma", "ggplot2", "plyr", "tidyr", "quantmod")
   lapply(required_packages, FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
       install.packages(x, dependencies = TRUE)
@@ -20,150 +20,150 @@ tsTrends <- function(in_ts, wind = 17, graph = T)
   #---------------------
   #per_ema <- 13
   #in_ts <- EMA(xts_cp[, 1], per_ema)
-  #in_ts <- scale(EMA(xts_cp[, 1], per_ema))
-  s_start <- wind + 1
+  #in_ts <- in_ts[-c(1:(per_ema - 1))]
+  s_start <- slope_window + 1
   s_end <- length(in_ts)
-  x <- c(1:(wind + 1))
+  x <- c(1:(slope_window + 1))
   dydx_mu <- c()
   dydx_sd <- c()
   t <- 0
   #---------------------
   for(s in s_start:s_end)
   {
-    datseg <- in_ts[(s - wind):s]
+    datseg <- in_ts[(s - slope_window):s]
     q <- polyfit(x, datseg, n = 2)
     a2 <- q[1]; a1 <- q[2]; a0 <- q[3]
     this_slope <- 2 * a2 * x + a1
     t <- t + 1
-    dydx_mu[t] <- mean(this_slope)
-    dydx_sd[t] <- sd(this_slope)
+    dydx_mu[t] <- mean(this_slope, na.rm = T)
+    dydx_sd[t] <- sd(this_slope, na.rm = T)
   }
   dydx_cv <- dydx_sd / dydx_mu
-  mu_dydx_mu <- mean(dydx_mu)
+  mu_dydx_cv <- mean(abs(dydx_cv), na.rm = T)
+  ldydx_cv <- log(abs(dydx_cv)) #EMA(log(abs(dydx_cv)), slope_window)
+  dydx_sd <- EMA(dydx_sd, slope_window)
+  mu_ldydx_cv <- mean(ldydx_cv, na.rm = T)
+  mu_dydx_mu <- mean(dydx_mu, na.rm = T)
   #---------------------
   #Identify periods of consecutively positive (for up trends)
   #or negative (for down trends) slope
   #---------------------
-  ind_downtrend <- which(dydx_mu > mu_dydx_mu) + round((per_ema + wind) / 2)
-  ts_up <- in_ts
-  ts_up[ind_downtrend] <- NA
-  ts_down <- rep(NA, nrow(in_ts))
-  ts_down[ind_downtrend] <- in_ts[ind_downtrend]
-  ind2_uptrend <- which(diff(ind_uptrend) > 1)
-  ind2_uptrend <- c(1, ind2_uptrend, length(ind_uptrend))
-  ts_mat <- cbind(in_ts, ts_up, ts_down)
-  
-  df <- fortify(ts_mat)
+  df <- fortify(in_ts)
+  colnames(df)[2] <- "ts"
   df$Index <- as.Date(df$Index)
-  colnames(df)[2:4] <- c("ts", "uptrend", "downtrend")
-  df_plot <- df %>% gather(series, value, ts:downtrend)
-  ggplot(df_plot, aes(x = Index, y = value, group = series, color = series)) + geom_line(size = 1.5)
-  
-  
+  df$dydxmu <- c(rep(NA, slope_window), dydx_mu)
+  df$dydxsd <- c(rep(NA, slope_window), dydx_sd)
+  df$ldydxcv <- c(rep(NA, slope_window), ldydx_cv)
   #---------------------
-  
-  
-  
-  
-  
-  
-  
-  
-  datevec<-as.numeric(gsub('-', '', datevec))
-  datevec<-as.Date(as.character(datevec),format="%Y%m%d")
+  ind_uptrnd <- which(df$dydxmu > mu_dydx_mu)
+  ind_dntrnd <- which(df$dydxmu < mu_dydx_mu)
+  df$ts_up <- NA
+  df$ts_dn <- NA
+  df$ts_up[ind_uptrnd] <- df$ts[ind_uptrnd]
+  df$ts_dn[ind_dntrnd] <- df$ts[ind_dntrnd]
+  #---------------------
+  ind_Selsig <- ind_uptrnd[which(diff(ind_uptrnd) != 1)]
+  ind_Buysig <- ind_dntrnd[which(diff(ind_dntrnd) != 1)]
+  n_b <- length(ind_Buysig)
+  n_s <- length(ind_Selsig)
+  if(ind_Buysig[1] > ind_Selsig[1]){ind_Selsig <- ind_Selsig[-1]; n_s <- length(ind_Selsig)}
+  if(ind_Buysig[n_b] > ind_Selsig[n_s]){ind_Buysig <- ind_Buysig[-n_b]; n_b <- length(ind_Buysig)}
+  #---------------------
+  df$RetLong_mag <- NA
+  df$RetLong_pct <- NA
+  df$timeLong <- NA
+  df$RetShrt_mag <- NA
+  df$RetShrt_pct <- NA
+  df$timeShrt <- NA
+  df$RetLong_mag[ind_Buysig] <- df$ts[ind_Selsig] - df$ts[ind_Buysig]
+  df$RetLong_pct[ind_Buysig] <- df$RetLong_mag[ind_Buysig] / df$ts[ind_Buysig]
+  df$timeLong[ind_Buysig] <- ind_Selsig - ind_Buysig
+  df$RetShrt_mag[ind_Selsig[-n_s]] <- -(df$ts[ind_Buysig[-1]] - df$ts[ind_Selsig[-n_s]])
+  df$RetShrt_pct[ind_Selsig[-n_s]] <- df$RetShrt_mag[ind_Selsig[-n_s]] / df$ts[ind_Selsig[-n_s]]
+  df$timeShrt[ind_Selsig[-n_s]] <- ind_Buysig[-1] - ind_Selsig[-n_s]
   #-----------------------
-  #   ema1<-EMA(indata,3)
-  #   indata<-ema1
-  out <- slopeOsc(indata,wind,graph1, getbinry = T)
-  slope<-out[[1]]
-  mu_slope<-out[[2]]
+  #Get date of trend start/finish points
+  df$BuysigDate <- NA
+  df$BuysigDate[ind_Buysig] <- df$Index[ind_Buysig]
+  df$BuysigDate <- as.Date(df$BuysigDate)
+  df$SelsigDate <- NA
+  df$SelsigDate[ind_Selsig] <- df$Index[ind_Selsig]
+  df$SelsigDate <- as.Date(df$SelsigDate)
   #-----------------------
-  #one way to do it...
-  ind_ut<-which(slope>mu_slope)-round(wind/2)
-  ind2_ut<-which(diff(ind_ut)>1)
-  ind2_ut<-c(1,ind2_ut,length(ind_ut))
+  #Create binary buy/sell var for ML model construction
+  #I.e. expand single buy/sell signal date to a window of a few days,
+  #with more days before the critical date than after it (before_window > aft_window)
+  df$Buysig <- 0
+  df$Selsig <- 0
+  for(i in 1:length(ind_Buysig)){df$Buysig[(ind_Buysig[i] - before_window):(ind_Buysig[i] + aft_window)] <- 1}
+  for(i in 1:length(ind_Selsig)){df$Selsig[(ind_Selsig[i] - before_window):(ind_Selsig[i] + aft_window)] <- 1}
+  df$Sig <- "Hold"
+  df$Sig[which(df$Selsig == 1)] <- "Sell"
+  df$Sig[which(df$Buysig == 1)] <- "Buy"
   #-----------------------
-  #another way...slope of slope (2nd derivative of EMA)
-  #   out2<-slopeOsc(slope,wind=3,graph=0);#out2<<-out2
-  #   slope2<-out2[[1]]
-  #   mu_slope2<-out2[[2]]
-  #   ind_ut<-which(slope2>mu_slope2)
-  #   ind2_ut<-which(diff(ind_ut)>1)
-  #   ind2_ut<-c(1,ind2_ut,length(ind_ut))
+  #Interesting... the up trend start (buy) points coincide with max ldydxcv points
+  #And these are all at about the same value (for SPY anyway)
+  ind_ind_muldydxcvBuy <- which(df$ldydxcv[ind_Buysig] > 0)
+  mu_ldydx_cvBuy <- mean(df$ldydxcv[ind_Buysig[ind_ind_muldydxcvBuy]])
+  sd_ldydx_cvBuy <- sd(df$ldydxcv[ind_Buysig[ind_ind_muldydxcvBuy]])
+  #---------------------------------
+  out_RetMagSum <- sum(df$RetLong_mag, na.rm = T)
+  out_RetMagMu <- mean(df$RetLong_mag, na.rm = T)
+  out_RetMagSd <- sd(df$RetLong_mag, na.rm = T)
+  out_RetPctMu <- mean(df$RetLong_pct, na.rm = T)    
+  out_RetPctSd <- sd(df$RetLong_pct, na.rm = T)
+  sd_dydx_mu <- sd(dydx_mu, na.rm = T)
+  outVars <- data.frame(muLdydxCVbuy = mu_ldydx_cvBuy, 
+                        sdLdydxCVbuy = sd_ldydx_cvBuy,
+                        mudydx = mu_dydx_mu,
+                        sddydx = sd_dydx_mu,
+                        RetMagSum = out_RetMagSum,
+                        RetMagMu = out_RetMagMu,
+                        RetMagSd = out_RetMagSd,
+                        RetPctMu = out_RetPctMu,
+                        RetPctSd = out_RetPctSd,
+                        n_uptrends = n_b)
+  #---------------------------------
+  #Output df with the original ts and its rolling slope series (mean, sd, cv),
+  #buy/sell binary var, etc.
+  df_ts <- df[, c("Index", "ts", "dydxmu", "dydxsd", "ldydxcv", "Sig")]
+  #Output df with only trend start/finish (buy/sell) point data
+  ind_trndBegEnd <- unique(c(which(is.na(df$BuysigDate) == F), which(is.na(df$SelsigDate) == F)))
+  df_tsCritPoints <- df[ind_trndBegEnd, 
+                      c("Index", "RetLong_pct", "RetShrt_pct", "RetLong_mag",
+                        "RetShrt_mag", "timeLong", "timeShrt", "dydxmu",
+                        "dydxsd", "ldydxcv")]
+  df_tsCritPoints <- df_tsCritPoints[order(df_tsCritPoints$Index), ]
+  #=================================
+  #2 Graphs
+  if(quietly == F){
+  #Plot of just the ts with trend start/finish (buy/sell) points overlaid
+  df_plot <- df[, c("Index", "BuysigDate", "SelsigDate", "RetLong_pct",
+                    "RetShrt_pct", "RetLong_mag", "RetShrt_mag",
+                    "ts_up", "ts_dn")]
+  df_plot <- df_plot %>% gather(trends, value, ts_up:ts_dn)
+  gg <- ggplot(df_plot, aes(x = Index, y = value, group = trends, color = trends))
+  gg <- gg + geom_line()
+  gg <- gg + geom_point(aes(x = SelsigDate, y = value, size = RetShrt_pct), color = "red")
+  gg <- gg + geom_point(aes(x = BuysigDate, y = value, size = RetLong_pct), color = "green")
+  print(gg)
   #-----------------------
-  price_range<-max(indata,na.rm=TRUE)-min(indata,na.rm=TRUE)
-  #print(price_range)
-  n_ut<-0
-  indxlist<-list()
-  magvec<-c()
-  magvecNormd <- c()
-  magvecNormd2 <- c()
-  #roughvec<-c()
-  for(i in 2:length(ind2_ut))
-  {
-    #  print(i)
-    ind<-c()
-    ind<-ind_ut[(ind2_ut[i-1]+1):ind2_ut[i]]
-    if(length(ind)==1){print("hello"); next}
-    if(ind[1]<8)
-    {
-      ind<-c(1:(ind[length(ind)]+8))
-    }
-    else if((ind[length(ind)]+8)>length(indata))
-    {
-      ind<-c((ind[1]-8):length(indata))
-    }
-    else
-    {    
-      ind<-c((ind[1]-8):(ind[length(ind)]+8))
-    }
-    u_beg<-which(indata[ind]==min(indata[ind],na.rm=TRUE))
-    u_end<-which(indata[ind]==max(indata[ind],na.rm=TRUE))
-    ind_beg<-ind[u_beg]
-    ind_end<-ind[u_end]
-    #  print(paste(ind_beg,ind_end))
-    if(ind_beg>ind_end){next}
-    min_thisRange <- min(indata[ind],na.rm=TRUE)
-    this_movemnt<-max(indata[ind],na.rm=TRUE)-min_thisRange
-    #  print(this_movemnt)
-    if(this_movemnt<.1*price_range){next}
-    #--Passed
-    n_ut<-n_ut+1
-    indxlist[[n_ut]]<-c(ind_beg:ind_end)
-    magvec[n_ut]<-this_movemnt
-    frac_increase <- this_movemnt / min_thisRange
-    magvecNormd[n_ut] <- frac_increase
-    this_slope <- this_movemnt / length(ind)
-    magvecNormd2[n_ut] <- frac_increase * this_slope
-    #roughvec[n_ut]<-sd(indata[ind])/mean(indata[ind])
+  #Plot roling slope (mean, sd, and cv) with trend start/finish (buy/sell) points overlaid
+  #The dydxmu plot includes the mean
+  #The ldydxcv plot includes the mean only of buy points > 0
+  df_plot <- df[, c("Index", "BuysigDate", "SelsigDate", "RetLong_pct", 
+               "RetShrt_pct", "RetLong_mag", "RetShrt_mag", 
+               "dydxmu", "dydxsd", "ldydxcv")]
+  df_plot <- df_plot %>% gather(ts_type, value, dydxmu:ldydxcv)
+  gg <- ggplot(df_plot, aes(x = Index, y = value)) + geom_line()
+  gg <- gg + geom_point(aes(x = SelsigDate, y = value, size = RetShrt_pct), color = "red")
+  gg <- gg + geom_point(aes(x = BuysigDate, y = value, size = RetLong_pct), color = "green")
+  gg <- gg + facet_wrap(~ts_type, ncol = 1, scales = "free")
+  gg <- gg + geom_hline(data = data.frame(yint = mu_dydx_mu, ts_type = "dydxmu"), aes(yintercept = yint), linetype = "dotted")
+  gg <- gg + geom_hline(data = data.frame(yint = mu_ldydx_cvBuy, ts_type = "ldydxcv"), aes(yintercept = yint), linetype = "dotted")
+  print(gg)
   }
-  print(paste("Num. Up-trends: ",n_ut))
-  #-----------------------
-  if(graph2==1)
-  {
-    #     windows()
-    par(mar=c(3,2.5,1,1.5));par(mfrow=c(2,1))
-    #indata<-zoo(indata)
-    indata<-xts(indata,datevec)
-    indata<-zoo(indata)
-    plot(indata,type="l",main="Data with trends")
-    #lines(indata,col="coral")
-    for(i in 1:length(indxlist))
-    {
-      ind<-c()
-      ind<-indxlist[[i]]
-      lines(indata[ind],col="green",lwd=2)
-    }
-    #--
-    slope<-as.zoo(slope)
-    plot(slope,type="l",main="EMA Slope Osc.")
-    abline(h=mu_slope,col="green")
-    #--
-    #     slope2<-as.zoo(slope2)
-    #     plot(slope2,type="l",main="2nd Order EMA Slope Osc.")
-    #     abline(h=mu_slope2,col="green")
-    
-  }
-  #return(list(indxlist,magvec,roughvec))
-  return(list(indxlist,magvec, magvecNormd, magvecNormd2))
+  #=================================
+  outlist <- list(outVars, df_tsCritPoints, df_ts)
+  return(outlist)
 }
